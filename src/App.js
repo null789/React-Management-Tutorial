b232ee0cfe8dd886be99c5dc6c58db4f01459241
null789 <eugene1593@gmail.com>
2022-01-21 11:34:33 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
+import Customer from './componets/Customer';
 import logo from './logo.svg';
 import './App.css';
-import Customer from './componets/Customer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import { withStyles } from '@mui/material/styles';
+
+
 
 const customers = [
   {
@@ -27,9 +36,21 @@ const customers = [
 class App extends Component{
     render(){
       return (
-        <div>
-          {customers.map(c=>{ return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} age={c.age}/>)})}
-        </div>
+        <Paper >
+          <Table >
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>나이</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customers.map(c=>{ return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} age={c.age}/>)})}
+            </TableBody>
+          </Table>
+        </Paper>
       );
   }
 }
--- src/componets/Customer.js
+++ src/componets/Customer.js
@@ -1,34 +1,17 @@
 import React from "react"; 
+import TableRow from '@mui/material/TableRow'
+import TableCell from '@mui/material/TableCell'
+
 
 class Customer extends React.Component{
     render(){
         return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo name={this.props.name} age={this.props.age}/>
-            </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component{
-    render() {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render() {
-        return(
-            <div>
-                <h2>{this.props.name}</h2>
-                <h2>{this.props.age}</h2>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.age}</TableCell>
+            </TableRow>
         )
     }
 }
